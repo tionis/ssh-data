8c61c590bb81bdea02c63f3f408543512c7e1a3f
tionis <git@tionis.dev>
2024-07-28 03:34:41 +0200

    correctly export all authorized_key fields

--- util/authorized_keys.go
+++ util/authorized_keys.go
@@ -11,23 +11,23 @@ import (
 // AuthorizedKey represents an SSH authorized_keys entry
 type AuthorizedKey struct {
 	Key             ssh.PublicKey
-	comment         string
+	Comment         string
 	Principals      []string
 	IsCA            bool
-	command         sql.NullString
-	environment     map[string]string
-	expiryTime      sql.NullTime
-	agentForwarding bool
-	from            []*Pattern
+	Command         sql.NullString
+	Environment     map[string]string
+	ExpiryTime      sql.NullTime
+	AgentForwarding bool
+	From            []*Pattern
 	PortForwarding  bool
-	pty             bool
+	Pty             bool
 	UserRC          bool
 	X11Forwarding   bool
-	permitListen    sql.NullString
-	permitOpen      sql.NullString
-	noTouchReq      bool
-	verifyReq       bool
-	tunnel          sql.NullString
+	PermitListen    sql.NullString
+	PermitOpen      sql.NullString
+	NoTouchReq      bool
+	VerifyReq       bool
+	Tunnel          sql.NullString
 }
 
 func (k *AuthorizedKey) MatchesPrincipal(input string) bool {
@@ -42,35 +42,35 @@ func (k *AuthorizedKey) MatchesPrincipal(input string) bool {
 func NewAuthorizedKey(key ssh.PublicKey, comment string, options []string) (*AuthorizedKey, error) {
 	ak := &AuthorizedKey{
 		Key:        key,
-		comment:    comment,
+		Comment:    comment,
 		Principals: []string{},
 		IsCA:       false,
-		command: sql.NullString{
+		Command: sql.NullString{
 			String: "",
 			Valid:  false,
 		},
-		environment: map[string]string{},
-		expiryTime: sql.NullTime{
+		Environment: map[string]string{},
+		ExpiryTime: sql.NullTime{
 			Time:  time.Time{},
 			Valid: false,
 		},
-		agentForwarding: true,
-		from:            []*Pattern{},
+		AgentForwarding: true,
+		From:            []*Pattern{},
 		PortForwarding:  true,
-		pty:             true,
+		Pty:             true,
 		UserRC:          true,
 		X11Forwarding:   true,
-		permitListen: sql.NullString{
+		PermitListen: sql.NullString{
 			String: "",
 			Valid:  false,
 		},
-		permitOpen: sql.NullString{
+		PermitOpen: sql.NullString{
 			String: "",
 			Valid:  false,
 		},
-		noTouchReq: false,
-		verifyReq:  false,
-		tunnel: sql.NullString{
+		NoTouchReq: false,
+		VerifyReq:  false,
+		Tunnel: sql.NullString{
 			String: "",
 			Valid:  false,
 		},
@@ -78,15 +78,15 @@ func NewAuthorizedKey(key ssh.PublicKey, comment string, options []string) (*Aut
 	for _, option := range options {
 		switch option {
 		case "agent-forwarding":
-			ak.agentForwarding = true
+			ak.AgentForwarding = true
 		case "cert-authority":
 			ak.IsCA = true
 		case "no-agent-forwarding":
-			ak.agentForwarding = false
+			ak.AgentForwarding = false
 		case "no-port-forwarding":
 			ak.PortForwarding = false
 		case "no-pty":
-			ak.pty = false
+			ak.Pty = false
 		case "no-user-rc":
 			ak.UserRC = false
 		case "no-x11-forwarding":
@@ -94,19 +94,19 @@ func NewAuthorizedKey(key ssh.PublicKey, comment string, options []string) (*Aut
 		case "port-forwarding":
 			ak.PortForwarding = true
 		case "pty":
-			ak.pty = true
+			ak.Pty = true
 		case "no-touch-required":
-			ak.noTouchReq = true
+			ak.NoTouchReq = true
 		case "verify-required":
-			ak.verifyReq = true
+			ak.VerifyReq = true
 		case "user-rc":
 			ak.UserRC = true
 		case "X11-forwarding":
 			ak.X11Forwarding = true
 		case "restrict":
-			ak.agentForwarding = false
+			ak.AgentForwarding = false
 			ak.PortForwarding = false
-			ak.pty = false
+			ak.Pty = false
 			ak.UserRC = false
 			ak.X11Forwarding = false
 		default:
@@ -119,12 +119,12 @@ func NewAuthorizedKey(key ssh.PublicKey, comment string, options []string) (*Aut
 				}
 				switch command {
 				case "command":
-					ak.command.Valid = true
-					ak.command.String = value
+					ak.Command.Valid = true
+					ak.Command.String = value
 				case "environment":
 					envParts := strings.SplitN(value, "=", 2)
 					if len(envParts) == 2 {
-						ak.environment[envParts[0]] = envParts[1]
+						ak.Environment[envParts[0]] = envParts[1]
 					} else {
 						return nil, fmt.Errorf("invalid environment option: %s", value)
 					}
@@ -133,8 +133,8 @@ func NewAuthorizedKey(key ssh.PublicKey, comment string, options []string) (*Aut
 					if err != nil {
 						return nil, fmt.Errorf("invalid expiry-time: %s", value)
 					}
-					ak.expiryTime.Valid = true
-					ak.expiryTime.Time = timespec
+					ak.ExpiryTime.Valid = true
+					ak.ExpiryTime.Time = timespec
 				case "from":
 					parts := strings.Split(value, ",") // ssh_config man page doesn't specify escaping rules, so we'll just split on commas
 					for _, part := range parts {
@@ -142,21 +142,21 @@ func NewAuthorizedKey(key ssh.PublicKey, comment string, options []string) (*Aut
 						if err != nil {
 							return nil, fmt.Errorf("invalid from option: %s", value)
 						}
-						ak.from = append(ak.from, pattern)
+						ak.From = append(ak.From, pattern)
 
 					}
 				case "permit-listen":
-					ak.permitListen.Valid = true
-					ak.permitListen.String = value
+					ak.PermitListen.Valid = true
+					ak.PermitListen.String = value
 				case "permit-open":
-					ak.permitOpen.Valid = true
-					ak.permitOpen.String = value
+					ak.PermitOpen.Valid = true
+					ak.PermitOpen.String = value
 				case "principal":
 					parts := strings.Split(value, ",") // ssh_config man page doesn't specify escaping rules, so we'll just split on commas
 					ak.Principals = append(ak.Principals, parts...)
 				case "tunnel":
-					ak.tunnel.Valid = true
-					ak.tunnel.String = value
+					ak.Tunnel.Valid = true
+					ak.Tunnel.String = value
 				default:
 					return nil, fmt.Errorf("unknown option: %s", option)
 				}
